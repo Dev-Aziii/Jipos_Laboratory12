1377857455673379223d6a74864736cf2f4a306c
Dev-Aziii <145265715+Dev-Aziii@users.noreply.github.com>
2025-04-21 22:35:58 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\AuthController;
 
-<<<<<<< HEAD
-Route::get('/', function () {
-    return view('home');
-});
-
-=======
 Route::get('/', [PostController::class, 'index']);
->>>>>>> 26e4a0607da03f91d78c82d181ec7e630c083c86
+
 Route::resource('posts', PostController::class);
-
-Route::get('/register', [AuthController::class, 'showRegister'])->name('showRegister');
-Route::post('/register', [AuthController::class, 'register'])->name(name: 'registration.post');
-
-
-Route::get('/login', [AuthController::class, 'showLogin'])->name('showLogin');
-Route::post('/login', [AuthController::class, 'login'])->name(name: 'login.post');
-
-
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
